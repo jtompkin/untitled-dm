e40f211da589d1193cfdb7c9f276ec4548a2ad29
jtompkin <jtompkin-dev@pm.me>
2026-02-06 13:24:43 -0500

    Big ol update and add TOML config

--- untitled-dm.go
+++ untitled-dm.go
@@ -1,40 +1,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 
+	"github.com/BurntSushi/toml"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// model implements tea.Model
-type model struct {
-	// choices is the list of choices that the user can choose from.
-	choices []string
-	// cmds maps indices in choices to executables to run.
-	cmds map[int]struct {
+type (
+	// model implements tea.Model
+	model struct {
+		// choices is the list of choices that the user can choose from.
+		choices []string
+		// cmds maps indices in choices to executables to run.
+		cmds map[int]command
+		// output contains the combined standard streams of the command and any error that occurred
+		output struct {
+			std string
+			err error
+		}
+		// cursor is the index of the current cursor position in choices.
+		cursor int
+		// selected is the index of selected item in choices. No items are selected if
+		// negative
+		selected int
+	}
+	command struct {
+		// name is the file name of the command
 		name string
 		args []string
 	}
-	// err is any error that occurred while running a command from cmds.
-	err error
-	// cursor is the index of the current cursor position in choices.
-	cursor int
-	// selected is the index of selected item in choices. No items are selected if
-	// negative
-	selected int
-}
+	// config is the TOML structure of the configuration file
+	config struct {
+		Commands []struct {
+			// Name is the string that is presented in the menu
+			Name string
+			// Command is the file name to be run
+			Command string
+			// Args are passed to Command
+			Args []string
+		}
+	}
+	// ranMsg indicates that the command successfully completed
+	ranMsg struct{ output string }
+	// errorMsg indicates that the command did not successfully complete
+	errorMsg struct {
+		output string
+		err    error
+	}
+)
 
 func (m model) Init() tea.Cmd { return nil }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case ranMsg:
+		m.output.std = msg.output
 		return m, tea.Quit
 	case errorMsg:
-		m.err = msg
+		m.output.std = msg.output
+		m.output.err = msg.err
 		return m, tea.Quit
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -70,11 +100,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	if m.err != nil {
-		return fmt.Sprintf("\nCould not start selected option: %v\n\n", m.err)
+	if m.output.err != nil {
+		return fmt.Sprintf("\nCould not start selected option: %v\n\n%s", m.output.err, m.output.std)
 	}
-	var b strings.Builder
-	b.WriteString("Where should we go?\n\n")
+	b := new(strings.Builder)
+	b.WriteString("Where should we go?\nPress enter to confirm selection.\n\n")
 	for i, choice := range m.choices {
 		cursor := " "
 		if m.cursor == i {
@@ -84,47 +114,62 @@ func (m model) View() string {
 		if m.selected == i {
 			checked = "x"
 		}
-		fmt.Fprintf(&b, "%s [%s] %s\n", cursor, checked, choice)
+		fmt.Fprintf(b, "%s [%s] %s\n", cursor, checked, choice)
 	}
-	b.WriteString("\nPress q to quit.\n")
+	fmt.Fprintf(b, "\nPress q to quit.\n%s", m.output.std)
 	return b.String()
 }
 
 func runShCmd(cmd *exec.Cmd) tea.Cmd {
 	return func() tea.Msg {
 		if cmd == nil {
-			return ranMsg{}
+			return ranMsg{"No command to run\n"}
 		}
-		if err := cmd.Run(); err != nil {
-			return errorMsg{err}
+		o, err := cmd.CombinedOutput()
+		if err != nil {
+			return errorMsg{output: string(o), err: err}
 		}
-		return ranMsg{}
+		return ranMsg{string(o)}
 	}
 }
 
-// ranMsg indicates that the command successfully completed
-type ranMsg struct{}
-
-// errorMsg indicates that the command did not successfully complete
-type errorMsg struct{ err error }
-
-func (e errorMsg) Error() string { return e.err.Error() }
-
-func initialModel() model {
-	return model{
-		choices: []string{"Xinit", "TTY"},
-		cmds: map[int]struct {
-			name string
-			args []string
-		}{
-			0: {name: "startx"},
-		},
-		selected: -1,
+func parseTomlConfig(f string) (conf config) {
+	if _, err := os.Stat(f); err != nil {
+		return conf
 	}
+	if _, err := toml.DecodeFile(f, &conf); err != nil {
+		log.Fatal(err)
+	}
+	return conf
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	var confFlag = flag.String("c", "config.toml", "Path to configuration file")
+	flag.Parse()
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.txt", "debug")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+	conf := parseTomlConfig(*confFlag)
+	m := model{
+		choices:  make([]string, len(conf.Commands)),
+		cmds:     make(map[int]command),
+		selected: -1,
+	}
+	for i, c := range conf.Commands {
+		m.choices[i] = c.Name
+		if c.Command != "" {
+			m.cmds[i] = command{
+				name: c.Command,
+				args: c.Args,
+			}
+		}
+	}
+	p := tea.NewProgram(m)
 	_, err := p.Run()
 	if err != nil {
 		log.Fatal(err)
